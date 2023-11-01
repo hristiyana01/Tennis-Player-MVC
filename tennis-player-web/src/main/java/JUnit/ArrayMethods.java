2a299337e2a909d1ef28512d486739116fa7b4fc
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-11-01 08:22:45 +0200

    first class initializing

---
+++ tennis-player-web/src/main/java/JUnit/ArrayMethods.java
@@ -0,0 +1,14 @@
+package JUnit;
+
+public class ArrayMethods {
+    int findIndex(int[] array, int number) {
+        //...
+        return index;
+    }
+
+    boolean searchForNumber(int[] array, int numberToSearchFor) {
+        boolean found = false;
+        //...
+        return found;
+    }
+}
